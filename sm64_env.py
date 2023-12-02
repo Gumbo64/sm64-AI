9cde84bf08e2cf0c20ff68d849e487c54951e3c3
Gumbo64 <chickennuggetcrazykid@gmail.com>
2023-12-02 16:06:24 +1100

    env actions

--- sm64_env.py
+++ sm64_env.py
@@ -6,6 +6,7 @@ from PIL import Image
 import numpy as np
 from gymnasium.spaces import Discrete, MultiDiscrete
 import functools
+import math
 
 class gfxPixels(ctypes.Structure):
     _fields_ = [
@@ -21,16 +22,61 @@ class inputStruct(ctypes.Structure):
         ("buttonInput",ctypes.c_bool * 3), # goes A,B,Z
     ]
 
+def make_action(angleDegrees,A,B,Z):
+    inputstruct = inputStruct()
+    if angleDegrees == "noStick":
+        inputstruct.stickX = 0
+        inputstruct.stickY = 0
+    else:
+        inputstruct.stickX = round(64 * math.sin(angleDegrees * math.pi/180))
+        inputstruct.stickY = round(64 * math.cos(angleDegrees * math.pi/180))
+    inputstruct.buttonInput = (ctypes.c_bool * 3)(A,B,Z)
+    return inputstruct
+
 class SM64_ENV(ParallelEnv):
     metadata = {
         "name": "sm64",
     }
 
     def __init__(self):
-        self.action_spaces = {
-            "mario": Discrete(12),
-        }
+        # angleDegrees, A, B, Z
+        # if angleDegrees == "noStick" then there is no direction held
+        self.action_book = [
+            # -----FORWARD
+            # None
+            make_action(0,False,False,False),
+            # Jump
+            make_action(0,True,False,False),
+            # Longjump
+            make_action(0,True,False,True),
+            # Dive
+            make_action(0,False,True,False),
 
+            # -----FORWARD RIGHT
+            # None
+            make_action(30,False,False,False),
+            # Jump
+            make_action(30,True,False,False),
+
+            # -----FORWARD LEFT
+            # None
+            make_action(-30,False,False,False),
+            # Jump
+            make_action(-30,True,False,False),
+
+            # -----BACKWARDS
+            # None
+            make_action(180,False,False,False),
+            # Jump
+            make_action(180,True,False,False),
+
+            # ----- NO STICK (no direction held)
+            # None
+            make_action("noStick",False,False,False),
+            # Groundpound
+            make_action("noStick",False,False,True),
+        ]
+        self.N_ACTIONS = len(self.action_book)
 
         self.MAX_PLAYERS = 4
         self.N_SCREENS_WIDTH = 5
@@ -48,7 +94,8 @@ class SM64_ENV(ParallelEnv):
         
         self.dll.main_func()
         for i in range(10):
-            self.step({})
+            actions = [random.randint(0,self.N_ACTIONS-1) for _ in range(self.MAX_PLAYERS)]
+            self.step(actions)
         print("making marios")
         self.dll.makemariolol()
         self.reset()
@@ -59,10 +106,8 @@ class SM64_ENV(ParallelEnv):
     def step(self, actions):
         inputstructs = (inputStruct * self.MAX_PLAYERS)()
         for i in range(self.MAX_PLAYERS):
-            inputstructs[i].stickX = random.randint(-64, 64)
-            inputstructs[i].stickY = random.randint(-64, 64)
-            inputstructs[i].buttonInput = (ctypes.c_bool * 3)(*[random.choices([True, False],[39/40,1/40]) for _ in range(3)])
-            
+            inputstructs[i] = self.action_book[actions[i]]
+
         self.pixelPointers = self.dll.step_pixels(inputstructs)
         for i in range(self.MAX_PLAYERS):
             pixelStruct = self.pixelPointers[i].contents
@@ -97,8 +142,14 @@ if __name__ == "__main__":
     done = False
 
     while not done:
-        for i in range(100):
-            actions = {}
+        for i in range(1000000):
+            actions = [0 for _ in range(env.MAX_PLAYERS)]
+            if i % 10 == 0:
+                actions = [2 for _ in range(env.MAX_PLAYERS)]
+                
+
+            
+            # actions = [random.randint(0,env.N_ACTIONS-1) for _ in range(env.MAX_PLAYERS)]
             env.step(actions)
             env.render()
         print("RESET")
